27467e1ea8a4f498bb26b5dc3209f9ab43871efe
Steven Sajja <Stevensajja@gmail.com>
2015-05-07 22:32:29 +0200

    Heroku Keys changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,9 +46,9 @@ Rails.application.configure do
   config.paperclip_defaults = {
     :storage => :s3,
     :s3_credentials => {
-      :bucket => ENV['sajjapin'],
-      :access_key_id => ENV['AKIAIFYUSEWCSNFB4FPA'],
-      :secret_access_key => ENV['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']
+      :bucket => ENV['AWS_BUCKET'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
   }
   # Use the lowest log level to ensure availability of diagnostic information
